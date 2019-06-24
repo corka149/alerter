3a4b66a64cc0563a1826162ca8f8168c502fb92c
corka149 <corka149@mailbox.org>
2019-06-24 17:50:00 +0200

    Added test to assert stub mode of GPIO

--- test/alerter_test.exs
+++ test/alerter_test.exs
@@ -1,4 +1,13 @@
 defmodule AlerterTest do
   use ExUnit.Case
 
+  alias Circuits.GPIO
+
+  test "info returns a map" do
+    info = GPIO.info()
+
+    assert is_map(info)
+    assert info.name == :stub
+    assert info.pins_open == 0
+  end
 end
